52c18dce2121e4d0f30126a6dfbeaf1661f955a4
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-07-31 13:38:56 +0900

    20220731

---
+++ Team_FE&BE/All/SpringBoot/restapi/src/main/java/com/farmsecurity/restapi/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.farmsecurity.restapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.farmsecurity.restapi.model.Member;
+
+import java.util.List;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, String> {
+
+    List<Member> findByNameAndPhone(String name, String phone); // 아이디 찾기
+    List<Member> findByIdAndPhone(String id, String phone); // 비밀번호 찾기
+}
