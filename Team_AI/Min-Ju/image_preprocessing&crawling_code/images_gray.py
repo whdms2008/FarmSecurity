6e9968da939b873bb53292ed4c5af3b40172b7bc
freenozero <freenozero@naver.com>
2023-04-11 23:30:20 +0900

    d

--- Team_AI/Min-Ju/image_preprocessing&crawling_code/images_gray.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-file_path = r"C:\Users\freet\Desktop\capstone\final"
-file_name = os.listdir(file_path)
-
-for name in file_name:
-    img = Image.open(file_path + "/"+ name)
-    imgGray = img.convert('L')
-    imgGray.save(file_path + "/"+ name)
